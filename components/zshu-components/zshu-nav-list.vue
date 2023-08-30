f4f906e1d1aa2f1f7b604bad89c306f219f76caf
辶开呀 <914044753@qq.com>
2023-08-30 18:15:57 +0800

    修改nav-list

---
+++ components/zshu-components/zshu-nav-list.vue
@@ -0,0 +1,103 @@
+<template>
+  <view class="section-wrapper border_bottom_padding">
+    <view class="section-wrapper__left">
+      <image class="left-icon-image" v-if="item.iconUrl" :src="item.iconUrl"></image>
+      <text class="left-text">{{ item.text }}</text>
+    </view>
+    <view class="section-wrapper__middle"></view>
+    <view class="section-wrapper__right" @click="navigateTo(item.pagePath)">
+      <view class="right-text" v-if="item.rightText">{{ item.rightText }}</view>
+      <uni-icons type="right" :color="color" size="18"></uni-icons>
+    </view>
+
+  </view>
+
+</template>
+
+<script setup>
+
+
+import {navigateTo} from "@/utils/tools";
+
+const props = defineProps({
+
+  color: {
+    type: String,
+    default: '919199'
+  },
+
+  item: {
+    type: Object,
+    default: () => ({
+      iconUrl: 'https://xcx.jxgxsmt.com/static/images/mine/icon-list-item-1.png',
+      text: '我的收藏',
+      rightText: '',
+      pagePath: ''
+    })
+  }
+
+})
+
+
+</script>
+<style scoped lang="scss">
+
+.border_bottom_padding:after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 1px;
+  width: calc(100% - (2 * var(--padding)));
+  background-color: red;
+}
+
+.section-wrapper {
+  --padding: 20rpx;
+
+  display: flex;
+  align-items: center;
+  gap: 20rpx;
+  padding: 20rpx var(--padding);
+  box-sizing: border-box;
+  position: relative;
+  background-color: #fff;
+}
+
+.section-wrapper__left {
+  display: flex;
+  align-items: center;
+  position: relative;
+  gap: 20rpx;
+
+  .left-icon-image {
+    display: block;
+    width: 80rpx;
+    height: 80rpx;
+  }
+
+  .left-text {
+    font-size: 14px;
+    color: #303033;
+
+  }
+}
+
+.section-wrapper__middle {
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  //border: 1px solid blue;
+  flex: 1;
+}
+
+.section-wrapper__right {
+  align-self: stretch;
+  display: flex;
+  align-items: center;
+  border: 1px solid red;
+}
+</style>
+
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -13,13 +13,13 @@
     <view class="flex-column-container container">
 
 
-      <zshu-list-item-container v-for="item in list" :item="item" :key="item.id" />
+      <zshu-nav-list v-for="item in list" :item="item" :key="item.id" />
 
 
 
       <button @click="navigateTo('pages/page1/page1')">page1</button>
-      <button @click="navigateTo('pages/pages2/pages2')">page2</button>
-      <button @click="navigateTo('pages/pages3/pages3')">page3</button>
+<!--      <button @click="navigateTo('pages/pages2/pages2')">page2</button>-->
+<!--      <button @click="navigateTo('pages/pages3/pages3')">page3</button>-->
 
 
 
@@ -38,12 +38,12 @@ import {baseImgURL} from "@/http/config";
 
 const list = [
   {id: 1, iconUrl: baseImgURL + '/mine/icon-list-item-1.png', text: '我的收藏', pagePath: 'subpages_mine/my_collections'},
-  {id: 2, iconUrl: baseImgURL + '/mine/icon-list-item-2.png', text: '浏览记录', pagePath: 'subpages_mine/browse_records'},
-  {id: 3, iconUrl: baseImgURL + '/mine/icon-list-item-3.png', text: '我的名片', pagePath: 'subpages_mine/roles'},
-  {id: 4, iconUrl: baseImgURL + '/mine/icon-list-item-4.png', text: '名片记录', pagePath: 'subpages_mine/business_card_record'},
-  {id: 6, iconUrl: baseImgURL + '/mine/icon-list-item-6.png', text: '我的订单', pagePath: 'subpages_mine/my_orders'},
-  {id: 7, iconUrl: baseImgURL + '/mine/icon-list-item-7.png', text: '推广申请', pagePath: 'subpages_mine/promotion_request'},
-  {id: 8, iconUrl: baseImgURL + '/mine/icon-list-item-8.png', text: '联系平台', pagePath: 'subpages_mine/contact_platform'},
+  // {id: 2, iconUrl: baseImgURL + '/mine/icon-list-item-2.png', text: '浏览记录', pagePath: 'subpages_mine/browse_records'},
+  // {id: 3, iconUrl: baseImgURL + '/mine/icon-list-item-3.png', text: '我的名片', pagePath: 'subpages_mine/roles'},
+  // {id: 4, iconUrl: baseImgURL + '/mine/icon-list-item-4.png', text: '名片记录', pagePath: 'subpages_mine/business_card_record'},
+  // {id: 6, iconUrl: baseImgURL + '/mine/icon-list-item-6.png', text: '我的订单', pagePath: 'subpages_mine/my_orders'},
+  // {id: 7, iconUrl: baseImgURL + '/mine/icon-list-item-7.png', text: '推广申请', pagePath: 'subpages_mine/promotion_request'},
+  // {id: 8, iconUrl: baseImgURL + '/mine/icon-list-item-8.png', text: '联系平台', pagePath: 'subpages_mine/contact_platform'},
 ]
 
 </script>
